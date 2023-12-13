293e30929221704d000e15fc9ed3e337e7e465c6
Hao Liu <33023091+Human9000@users.noreply.github.com>
2023-12-13 17:49:32 +0800

    Create MHAConvSE.py

---
+++ MHAConvSE.py
@@ -0,0 +1,18 @@
+
+
+class SE(nn.Module):
+    def __init__(self, inp, oup, expansion=0.25):
+        super().__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(oup, int(inp * expansion), bias=False),
+            nn.GELU(),
+            nn.Linear(int(inp * expansion), oup, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
