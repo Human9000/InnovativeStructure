1cc2690a58766080cab3d0f65f5602105cc01af1
Hao Liu <33023091+Human9000@users.noreply.github.com>
2025-04-14 16:14:09 +0800

    Update vig.py

--- vig/vig.py
+++ vig/vig.py
@@ -1,8 +1,8 @@
-from mingru import minGRU
-from einops import rearrange
+from mingru import minGRU 
 import torch
 from torch import nn
 from torch.nn import Module
+
 
 
 class ViGBlock(Module):
@@ -25,25 +25,29 @@ class ViGBlock(Module):
         self.c_unpatch_embadding = nn.ConvTranspose2d(dim, channle, kernel_size=patch_size, stride=patch_size)
 
     def forward(self, x):
-        # ======== 行列分别patch_embadding============
-        r = rearrange(self.r_patch_embadding(x),  # 行卷积，将相邻的patch_size行，升维到通道上，即patch_size行的通道数变为dim，保持列数不变，行数缩小patch_size倍，后续在列数上做minGRU
-                      'B (H d) r c -> B H c r d', H=self.head)  # 行 patch
+        # ======== 行列分别patch_embadding============  
+        r =self.r_patch_embadding(x).unflatten(1, (self.head, -1)).permute(0, 1, 4, 3, 2)
+        c =self.c_patch_embadding(x).unflatten(1, (self.head, -1)).permute(0, 1, 3, 4, 2)
+        rs3 = r.shape[:3]
+        cs3 = c.shape[:3]
+        c = c.flatten(0, 2) # (bhr, c, d//h)
+        r = r.flatten(0, 2) # (bhc, r, d//h)
 
-        c = rearrange(self.c_patch_embadding(x),  # 列卷积，将相邻的patch_size列，升维到通道上，即patch_size列的通道数变为dim，保持行数不变，列数缩小patch_size倍，后续在行数上做minGRU
-                      'B (H d) r c -> B H r c d', H=self.head)  # 列 patch 
+ 
         # ======= 行列正向GRU 和 逆向GRu =======
         r = self.r_gru_p(r)
         c = self.c_gru_p(c)
 
-        if self.r_gru_n is not None:  # 逆向
+        if self.r_gru_n is not None:  # 逆向 
             r = self.r_gru_p(r) + self.r_gru_n(r.flip(dims=[-1])).flip(dims=[-1])
             c = self.c_gru_p(c) + self.c_gru_n(c.flip(dims=[-1])).flip(dims=[-1])
 
         # ======= 行/列 unpatch_embadding=======
-        r = self.r_unpatch_embadding(rearrange(r, 'B H c r d -> B (H d) r c'))  # 行反卷积
-        c = self.c_unpatch_embadding(rearrange(c, 'B H r c d -> B (H d) r c'))  # 列反卷积
+        r = r.unflatten(0, rs3).permute(0, 1, 4, 3, 2).flatten(1,2) # (b, h*d//h, r, c)
+        c = c.unflatten(0, cs3).permute(0, 1, 4, 2, 3).flatten(1,2) # (b, h*d//h, r, c)
+        r = self.r_unpatch_embadding(r)  # 行 (b, c, H, W)
+        c = self.c_unpatch_embadding(c)  # 列 (b, c, H, W)
 
-        # ======= 残差融合 =====================
-        # y = self.residual_out(torch.cat([r, c, x], dim=1))
-        y = r+c+x
+        # ======= 残差融合 ===================== 
+        y = r + c + x
         return y
